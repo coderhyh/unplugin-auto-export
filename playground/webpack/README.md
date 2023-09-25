b497edc2f98561a5d42bc7583952768515f6bd03
coderhyh <772567615@qq.com>
2023-09-25 20:04:44 +0800

    docs: playground

--- playground/webpack/README.md
+++ playground/webpack/README.md
@@ -1,19 +1,19 @@
-# demooooo
+# Webpack Example
 
 ## Project setup
-```
+
+```bash
 npm install
 ```
 
 ### Compiles and hot-reloads for development
-```
+
+```bash
 npm run serve
 ```
 
 ### Compiles and minifies for production
-```
+
+```bash
 npm run build
 ```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- playground/webpack/vue.config.js
+++ playground/webpack/vue.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 const AutoExport = require('unplugin-auto-export/webpack').default
+
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
-      AutoExport({ 
-        path: ['@/components/*'],
+      AutoExport({
+        path: ['src/components/*'],
         componentDirs: ['components'],
       }),
     ],
